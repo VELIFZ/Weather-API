c4cb24a55bbabf505fac8e3fdf34c3018c4e490d
VELIFZ <eliferik72@gmail.com>
2022-03-29 02:26:08 -0400

    deneme

--- main.js
+++ main.js
@@ -1,67 +1,48 @@
-// 1 ilk once mutlaka api'yin calisip calismadigina bak
+function getData() {
+    let city = document.querySelector('#city').value;
+    const API_KEY = '';
+    console.log(city);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
 
-let getWeatherData = async (city) => {
-    let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4f628866358be722e72afa5f6680078b`)
-    let data = await response.json()
-    //console.log(data)
-    return await data
+        // icon
+        let icon = data['weather'][0]['icon'];
+        document.querySelector('#icon').innerHTML=`<img src="${icon}@2x.png">`;
+
+        // city name
+        let cityname = data['name'];
+        let country= data['sys']['country'];
+        document.querySelector('#cityname').innerHTML=(`${cityname}, ${country}`);
+        // card basligi
+        document.querySelector('#baslik').innerHTML=(`Weather in ${cityname}`);
+        console.log(cityname)
+        // current date
+        var today = new Date();
+        var currentDate = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
+        document.querySelector('#date').innerHTML=currentDate;
+
+        // description
+        let desc = data['weather'][0]['description'];
+        document.querySelector('#descr').innerHTML=desc;
+        // temp
+        let temp = Math.round(((data['main']['temp']-273.15)*1.8+32));
+        document.querySelector('#temp').innerHTML=`${temp}°F`;
+        // feels_like
+        let feels = data['main']['tfeels_like'];
+        document.querySelector('#feels').innerHTML=feels;
+        // Humidity
+        let humi = data['main']['humidity'];
+        document.querySelector('#humi').innerHTML=`${humi}%`;
+        // wind
+        let wind = data['wind']['speed'];
+        document.querySelector('#wind').innerHTML=`${wind} mph`;
+        // Max-Temp
+        let max_temp = Math.round(((data['main']['temp_max']-273.15)*1.8+32));
+        document.querySelector('#max_temp').innerHTML=`${max_temp}°F`;
+        // Min-Temp
+        let min_temp = Math.round(((data['main']['temp_min']-273.15)*1.8+32));
+        document.querySelector('#min_temp').innerHTML=`${min_temp}°F`;
+    })
 }
-
-//getWeatherData()
-
-// 2 aldigin datayi goster eventlistener ile baglayarak 
-    // ayni zamanda bu adimda html sayfasina yuklemek istedikelerini olustuyorsun. table,card gibi
-
-let loadWeatherData = async (city) => {
-    let data = await getWeatherData(city);
-    console.log(data);
-
-    // html'deki degisiklik yapacagin elemenleri sec
-
-
-}
-
-// 3 forma event listener ekle
-
-let form = document.querySelector('#weatherForm');
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    let city = e.target.city.value;
-    console.log(city)
-    let tahmin = await getWeatherData(city); // bunu aslinda ilk yaacaksin cunku ikinin icinde kullanilacak
-    console.log(tahmin);
-    loadWeatherData(tahmin);
-    form.reset();
-})
-
-
-// let form = document.querySelector('#weatherForm');
-// form.addEventListener('submit', async (e) => {
-//     e.preventDefault;
-//     let city = e.target.city.value;
-//     console.log(city);
-//     await loadData(city);
-//     form.reset();
-// });
-
-// let getData = async (city) => {
-//     try {
-//         let response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`);
-//         return response.data;
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-
-
-                // bunla gun vs yap
-
-// function todaydate() {
-//     var today_date = new Date();
-//     var myyear = today_date.getYear();
-//     var mymonth = today_date.getMonth() + 1;
-//     var mytoday = today_date.getDate();
-
-//     document.write("<h1>" + myyear + "/" + mymonth + "/"+mytoday + "/h1">);
-// }
